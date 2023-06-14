e0a62072f79c7a0a65907b079f9558d65679d0e2
Sage Paden <sage.paden@gmail.com>
2023-06-14 15:30:56 -0700

    cheeky lil commit
    Co-authored-by: Natalie Benjes <nataliebenjes@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,54 @@
 
 //Buisness logic for game
-function Game() {
-  this.players = {player1, player2};
-  this.currentId = 0;
 
+
+
+function Player(name) {
+  this.name = name;
+  this.score = 0;
 }
 
-Game.prototype.addPlayer = function(player) {
-  player.id = this.assignId();
-  this.players[player.id] = player;
-};
+let player1 = new Player("Player 1");
+let player2 = new Player("Player 2");
 
-Game.prototype.assignID = function() {
-  this.currentId += 1
-  return this.currentId;
-};
+function Game() {
+  this.players = {player1, player2};
+  this.activePlayer = player1;
+  this.currentScore = 0
+  this.gameover = false;
+  this.roll = 0
+  // this.currentId = 0;
+
+}
+  
+let game = new Game();
+
+
+Game.prototype.switchPlayers = function() {
+  this.activePlayer = this.activePlayer === player1 ? player2 : player1;
+  console.log(this.activePlayer);
+}
+
+Game.prototype.gameOver = function () {
+  if (this.score >= 100) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 Game.prototype.rollResults = function() {
   const min = Math.ceil(1);
   const max = Math.floor(7);
-  return Math.floor(Math.random() * (max - min) + min);
+  this.roll = Math.floor(Math.random() * (max - min) + min);
+  return this.roll
 
 }
 
 //Buisness logic for player
-
+// Player.prototype.addToScore = function() {
+  
+// }
 
 
 
@@ -35,6 +59,21 @@ Game.prototype.rollResults = function() {
 
 // Each turn, a player repeatedly rolls a die until either a 1 is rolled or the player decides to "hold":
 
+Game.prototype.rollScore = function() {
+  let roll = rollResults();
+  if (roll !== 1) {
+    this.currentScore += this.currentScore + roll;
+  } else {
+      this.currentScore = 0;
+  }
+  return this.currentScore;
+}
+
+//get this inside player logic. or not?function addToScore () {
+  // const randomNo = this.rollResults();
+  // this.score += randomNo;
+
+// game.players[0].addToScore();
 // If the player rolls a 1, they score nothing and it becomes the next player's turn.
 
 // If the player rolls any other number, it is added to their turn total and the player's turn continues.
